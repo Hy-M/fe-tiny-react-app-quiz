6c0441408355d154ea6af303eb4a950341929ef2
Alexander Trout <alexandertrout@A1.local>
2020-01-31 16:03:27 +0000

    completed quiz

--- src/components/Circles.jsx
+++ src/components/Circles.jsx
@@ -1,87 +1,19 @@
 import React from "react";
 
 const Circles = props => {
-  const { results1, results2, questionNumber } = props;
-  for (let i = 0; i < 5; i++) {
-    console.log(results1[i], results2[i]);
-  }
+  const { results1, results2, questionNumber, i, whichResults } = props;
 
   return (
-    <section>
-      <p>Player 1:</p>
-      <div
-        className={
-          results1[0] && results2[0]
-            ? results1[0]
-              ? results1[0]
-              : "circle"
+    <div
+      className={
+        results1[i] && results2[i]
+          ? whichResults[i]
+            ? whichResults[i]
             : "circle"
-        }
-        id={questionNumber}
-      ></div>
-      <div
-        className={
-          results1[1] && results2[1]
-            ? results1[1]
-              ? results1[1]
-              : "circle"
-            : "circle"
-        }
-        id={questionNumber}
-      ></div>
-      <div
-        className={
-          results1[2] && results2[2]
-            ? results1[2]
-              ? results1[2]
-              : "circle"
-            : "circle"
-        }
-        id={questionNumber}
-      ></div>
-      <div
-        className={
-          results1[3] && results2[3]
-            ? results1[3]
-              ? results1[3]
-              : "circle"
-            : "circle"
-        }
-        id={questionNumber}
-      ></div>
-      <div
-        className={
-          results1[4] && results2[4]
-            ? results1[4]
-              ? results1[4]
-              : "circle"
-            : "circle"
-        }
-        id={questionNumber}
-      ></div>
-
-      <p>Player 2:</p>
-      <div
-        className={results2[0] ? results2[0] : "circle"}
-        id={questionNumber}
-      ></div>
-      <div
-        className={results2[1] ? results2[1] : "circle"}
-        id={questionNumber}
-      ></div>
-      <div
-        className={results2[2] ? results2[2] : "circle"}
-        id={questionNumber}
-      ></div>
-      <div
-        className={results2[3] ? results2[3] : "circle"}
-        id={questionNumber}
-      ></div>
-      <div
-        className={results2[4] ? results2[4] : "circle"}
-        id={questionNumber}
-      ></div>
-    </section>
+          : "circle"
+      }
+      id={questionNumber}
+    ></div>
   );
 };
 
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -18,8 +18,8 @@ class Quiz extends Component {
         answers: [{ LOLCode: true }, { LollyGag: false }, { Ratoon: false }]
       },
       {
-        question: "Is Chris tarrant alive",
-        answers: [{ Yes: true }, { "Probably not": false }, { Maybe: false }]
+        question: "console.log( 1/ 0 ) = ??? ",
+        answers: [{ Infinity: true }, { NaN: false }, { undefined: false }]
       },
       {
         question: "How tall is Liam",
@@ -30,7 +30,29 @@ class Quiz extends Component {
         ]
       },
       {
-        question: "HOw tall is Dave",
+        question: "Which will return a random number between 1 and 3?",
+        answers: [
+          { "Math.floor(Math.random() * 3)": true },
+          { "Math.random(Math.floor() * 3)": false },
+          { "Math.floor(Math.random(3) * 10)": false }
+        ]
+      },
+
+      //LEVEL 2//
+      {
+        question: "Which is better, Mocha & Chai, Jest, Cucumber?",
+        answers: [{ "Mocha & Chai": true }, { Jest: true }, { Cucumber: false }]
+      },
+      {
+        question: "Yes?",
+        answers: [{ "what?": true }, { no: false }, { yes: false }]
+      },
+      {
+        question: "How many plants are in the northcoders office?",
+        answers: [{ 23: true }, { 15: false }, { "Too Many": false }]
+      },
+      {
+        question: "How tall is Dave",
         answers: [
           { "Really tall": true },
           { Tall: false },
@@ -38,41 +60,41 @@ class Quiz extends Component {
         ]
       },
       {
-        question: "q6?",
+        question: "console.log(0.1 + 0.2) gives what?",
+        answers: [{ 0.30000000000000004: true }, { 0.3: false }, { 3: false }]
+      },
+      //LEVEL 3//
+      {
+        question: "typeof Nan = ???",
+        answers: [{ Bread: false }, { Nan: false }, { Number: true }]
+      },
+      {
+        question: "hello ... ?",
         answers: [
-          { "Code on v8 engine": false },
-          { "Cats only validate emotions": false },
-          { "Closed over variable environment": true }
+          { "its me": false },
+          { world: true },
+          { "is it me you're looking for": false }
         ]
       },
       {
-        question: "q7?",
-        answers: [{ LOLCode: true }, { LollyGag: false }, { Ratoon: false }]
+        question: "{} + [] = ???",
+        answers: [{ 0: true }, { undefined: false }, { "{}[]": false }]
       },
       {
-        question: "q8",
-        answers: [{ Yes: true }, { "Probably not": false }, { Maybe: false }]
+        question: "When was Node.js invented",
+        answers: [{ "2008": false }, { "2009": true }, { "2010": false }]
       },
       {
-        question: "q9",
+        question: "[] + {}",
         answers: [
-          { Tall: true },
-          { "Really tall": false },
-          { "Quite tall": false }
-        ]
-      },
-      {
-        question: "q10",
-        answers: [
-          { Tall: true },
-          { "Really tall": false },
-          { "Quite tall": false }
+          { "[object Object]": true },
+          { "[Object]": false },
+          { undefined: false }
         ]
       }
     ],
     results1: [],
     results2: [],
-    // results: [],
     questionNumber: 0,
     roundIsFinished: false,
     levelNumber: 1,
@@ -139,6 +161,7 @@ class Quiz extends Component {
     } = this.state;
     let score1 = 0;
     let score2 = 0;
+    const circles = [0, 1, 2, 3, 4];
     results1.forEach(result => {
       if (result === "green") score1++;
     });
@@ -146,28 +169,63 @@ class Quiz extends Component {
       if (result === "green") score2++;
     });
 
+    if (questionNumber === 15) {
+      return (
+        <section>
+          <h1>GAME OVER</h1>
+          <img
+            src="https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2009/11/5/1257435057154/African-pygmy-hedgehog-008.jpg?width=700&quality=85&auto=format&fit=max&s=b46d65f210d2d799f0493dcb370d35ed"
+            alt="cutie"
+          />
+        </section>
+      );
+    }
+
     if (roundIsFinished === true) {
       return (
         <div>
           <h6>ROUND OVER</h6>
           <h3> Player 1 scored {score1} out of 5</h3>
           <h3> Player 2 scored {score2} out of 5</h3>
-          <Circles
-            results1={results1}
-            results2={results2}
-            questionNumber={questionNumber}
-          />
-          <button
-            onClick={() =>
-              this.setState({
-                roundIsFinished: false,
-                results1: [],
-                results2: []
-              })
-            }
-          >
-            Begin Level {levelNumber}
-          </button>
+          <p>Player 1:</p>
+          {circles.map(circle => {
+            return (
+              <Circles
+                key={circle}
+                results1={results1}
+                results2={results2}
+                questionNumber={questionNumber}
+                i={circle}
+                whichResults={results1}
+              />
+            );
+          })}
+          <p>Player 2:</p>
+          {circles.map(circle => {
+            return (
+              <Circles
+                key={circle}
+                results1={results1}
+                results2={results2}
+                questionNumber={questionNumber}
+                i={circle}
+                whichResults={results2}
+              />
+            );
+          })}
+          <div>
+            <button
+              onClick={() =>
+                this.setState({
+                  roundIsFinished: false,
+                  results1: [],
+                  results2: []
+                })
+              }
+            >
+              Begin Level {levelNumber}
+            </button>
+          </div>
         </div>
       );
     } else {
@@ -180,12 +238,32 @@ class Quiz extends Component {
             questionNumber={questionNumber}
             handleClick={this.handleClick}
           />
-
-          <Circles
-            results1={results1}
-            results2={results2}
-            questionNumber={questionNumber}
-          />
+          <p>Player 1:</p>
+          {circles.map(circle => {
+            return (
+              <Circles
+                key={circle}
+                results1={results1}
+                results2={results2}
+                questionNumber={questionNumber}
+                i={circle}
+                whichResults={results1}
+              />
+            );
+          })}
+          <p>Player 2:</p>
+          {circles.map(circle => {
+            return (
+              <Circles
+                key={circle}
+                results1={results1}
+                results2={results2}
+                questionNumber={questionNumber}
+                i={circle}
+                whichResults={results2}
+              />
+            );
+          })}
         </div>
       );
     }
