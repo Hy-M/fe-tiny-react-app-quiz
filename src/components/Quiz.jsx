65078d05c1d8e8fabc0866d98c011d8c6f1b4e37
Alexander Trout <alexandertrout@A1.local>
2020-01-31 16:22:57 +0000

    added images

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -11,7 +11,9 @@ class Quiz extends Component {
           { "Code on v8 engine": false },
           { "Cats only validate emotions": false },
           { "Closed over variable environment": true }
-        ]
+        ],
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/8/89/McWay_cove_1.jpg"
       },
       {
         question: "Which of these is a real programming language?",
@@ -27,7 +29,8 @@ class Quiz extends Component {
           { Tall: true },
           { "Really tall": false },
           { "Quite tall": false }
-        ]
+        ],
+        image: "https://ca.slack-edge.com/T1VHRHZE2-UQ5BL49SM-6eefb7cae9d6-512"
       },
       {
         question: "Which will return a random number between 1 and 3?",
@@ -41,7 +44,13 @@ class Quiz extends Component {
       //LEVEL 2//
       {
         question: "Which is better, Mocha & Chai, Jest, Cucumber?",
-        answers: [{ "Mocha & Chai": true }, { Jest: true }, { Cucumber: false }]
+        answers: [
+          { "Mocha & Chai": true },
+          { Jest: true },
+          { Cucumber: false }
+        ],
+        image:
+          "https://static1.squarespace.com/static/5a00e71a2278e7a804e532cf/5a306ed3ec212dec12e6b10f/5c90f1ace4966b3e9e8f6132/1553003074053/healthy-halal-meat-online-whole-cucumber.jpg"
       },
       {
         question: "Yes?",
@@ -57,7 +66,8 @@ class Quiz extends Component {
           { "Really tall": true },
           { Tall: false },
           { "Quite tall": false }
-        ]
+        ],
+        image: "https://ca.slack-edge.com/T1VHRHZE2-UQD9RN8EL-8b5e1f63ded7-512"
       },
       {
         question: "console.log(0.1 + 0.2) gives what?",
@@ -66,7 +76,9 @@ class Quiz extends Component {
       //LEVEL 3//
       {
         question: "typeof Nan = ???",
-        answers: [{ Bread: false }, { Nan: false }, { Number: true }]
+        answers: [{ Bread: false }, { Nan: false }, { Number: true }],
+        image:
+          "https://cdn.images.express.co.uk/img/dynamic/14/285x214/240295_1.jpg"
       },
       {
         question: "hello ... ?",
@@ -74,7 +86,9 @@ class Quiz extends Component {
           { "its me": false },
           { world: true },
           { "is it me you're looking for": false }
-        ]
+        ],
+        image:
+          "https://d2d2ekni7prhwy.cloudfront.net/wp-content/uploads/2020/01/Adele-press-shot.jpg"
       },
       {
         question: "{} + [] = ???",
@@ -85,12 +99,8 @@ class Quiz extends Component {
         answers: [{ "2008": false }, { "2009": true }, { "2010": false }]
       },
       {
-        question: "[] + {}",
-        answers: [
-          { "[object Object]": true },
-          { "[Object]": false },
-          { undefined: false }
-        ]
+        question: "How good is our quiz",
+        answers: [{ Fantastic: false }, { Amazing: false }, { undefined: true }]
       }
     ],
     results1: [],
@@ -238,6 +248,7 @@ class Quiz extends Component {
             questionNumber={questionNumber}
             handleClick={this.handleClick}
           />
+          <img src={quiz[questionNumber].image} alt=""></img>
           <p>Player 1:</p>
           {circles.map(circle => {
             return (
